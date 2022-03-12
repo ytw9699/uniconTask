40c5549ed54c3d67e41529e31d777fc6bc461063
champ_home <ytw9699@naver.com>
2022-03-12 22:16:52 +0900

    프로젝트 초기 생성

---
+++ uniconTask/src/test/java/org/task/processing/ProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.task.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
